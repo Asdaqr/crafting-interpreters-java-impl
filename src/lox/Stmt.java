ed75a3b7cf87317ac80bbe8c1ea789b58916c17c
Asdaq R <109673335+Asdaqr@users.noreply.github.com>
2025-08-09 23:08:25 -0400

    Built the statement portion

---
+++ src/lox/Stmt.java
@@ -0,0 +1,49 @@
+package lox;
+
+import java.util.List;
+
+public abstract class Stmt {
+
+    public interface Visitor<R> {
+        R visitExpressionStmt(Expression stmt);
+        R visitPrintStmt(Print stmt);
+        R visitVarStmt(Var stmt);
+    }
+    public static class Expression extends Stmt {
+        public Expression(Expr expression) {
+            this.expression = expression;
+        }
+
+        @Override
+        <R> R accept (Visitor<R> visitor) {
+            return visitor.visitExpressionStmt(this);
+        }
+        final Expr expression;
+    }
+    public static class Print extends Stmt {
+        public Print(Expr expression) {
+            this.expression = expression;
+        }
+
+        @Override
+        <R> R accept (Visitor<R> visitor) {
+            return visitor.visitPrintStmt(this);
+        }
+        final Expr expression;
+    }
+    public static class Var extends Stmt {
+        public Var(Token name, Expr initializer) {
+            this.name = name;
+            this.initializer = initializer;
+        }
+
+        @Override
+        <R> R accept (Visitor<R> visitor) {
+            return visitor.visitVarStmt(this);
+        }
+        final Token name;
+        final Expr initializer;
+    }
+
+    abstract <R> R accept(Visitor<R> visitor);
+}
